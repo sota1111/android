e1cd0830b7ae4e8026409fc7fd2e26f160aa91aa
sota1111 <sota.moro@gmail.com>
2023-02-17 23:45:44 +0900

    not display line

--- tetris_Euniclus/app/src/main/java/com/websarva/wings/android/tetris_euniclus/MainActivity.kt
+++ tetris_Euniclus/app/src/main/java/com/websarva/wings/android/tetris_euniclus/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.websarva.wings.android.tetris_euniclus
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +20,28 @@ class MainActivity : AppCompatActivity() {
             paint.style = Paint.Style.FILL
             paint.color = Color.argb(255, 255, 0, 255)
             canvas.drawRect(Rect(110, 210, 190, 290), paint)
+
+            // ライン表示
+            val wm: WindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            //val disp = wm.getDefaultDisplay()
+            val size = Point()
+            //disp.getSize(size)
+
+            val startX = 200f
+            val startY = 300f
+            val height = 1000
+            val sqSize = 700
+
+            paint.style = Paint.Style.STROKE
+
+            paint.color = Color.argb(255, 190, 200, 255)
+            paint.strokeWidth = 10f
+            canvas.drawLine(startX, startY+sqSize, startX+width, startY*height)
         }
 
     }
+}
+
+private fun Canvas.drawLine(startX: Float, fl: Float, fl1: Float, fl2: Float) {
+
 }
