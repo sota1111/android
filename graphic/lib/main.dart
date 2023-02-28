15d36de23691c43b105f3148fd0681f70dc11634
sota1111 <sota.moro@gmail.com>
2023-02-28 23:54:35 +0900

    Flutter入門　リスト5-1

--- graphic/lib/main.dart
+++ graphic/lib/main.dart
@@ -39,13 +39,35 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255,255,255,255),
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('App Name', style: TextStyle(fontSize: 30.0),),
       ),
-      body: Text(
-        widget.message,
-        style: TextStyle(fontSize:32.0)
-      )
+      body: Container(
+        child:CustomPaint(
+          painter: MyPainter(),
+        ),
+      ),
     );
   }
+}
+
+class MyPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size){
+    Paint p = Paint();
+    p.style = PaintingStyle.fill;
+    p.color = Color.fromARGB(150,0,200,255);
+    Rect r = Rect.fromLTWH(50, 50, 150, 150);
+    canvas.drawRect(r, p);
+
+    p.style = PaintingStyle.stroke;
+    p.color = Color.fromARGB(150, 200, 0, 255);
+    p.strokeWidth = 10.0;
+    r = Rect.fromLTWH(100.0, 100.0, 150.0, 150.0);
+    canvas.drawRect(r, p);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
