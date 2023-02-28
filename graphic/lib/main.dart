8b1431f4a4eca3c6c44320971fae9c79500a16d9
sota1111 <sota.moro@gmail.com>
2023-02-28 22:49:10 +0900

    Flutter入門　リスト2-3

---
+++ graphic/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+  final title = 'Graphic Sample';
+  final message = 'サンプル・メッセージ';
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(
+        title: this.title,
+        message: this.message
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  final String title;
+  final String message;
+  const MyHomePage({
+    Key? key,
+    required this.title,
+    required this.message
+  }): super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize:32.0)
+      )
+    );
+  }
+}
