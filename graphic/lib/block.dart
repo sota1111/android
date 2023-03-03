435e0e3d73fee7b054985652047e2586eea61fed
sota1111 <sota.moro@gmail.com>
2023-03-03 22:32:11 +0900

    Iブロックを作成

--- graphic/lib/block.dart
+++ graphic/lib/block.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 enum BlockMovement {
-  UP,
+  uP,
   DOWN,
   LEFT,
   RIGHT,
   ROTATE_CLOCKWISE,
   ROTATE_COUNTER_CLOCKWISE
+}
+
+class Block {
+  late int x;
+  late int y;
+  late Color color;
+  late List<List<int>> _IBlock;
+  Block(this.x, this.y, this.color,_IBlock){
+    _IBlock = [[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]];
+  }
+
 }
