d811ed517c825878da4d5164e24e33cd4e267af5
sota1111 <sota.moro@gmail.com>
2023-03-06 23:46:35 +0900

    Riverpodでカウント機能をprovide

--- test_Riverpod/lib/main.dart
+++ test_Riverpod/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = Provider((ref) => 'Hello World');
+final counterProvider = StateProvider<int>((ref) => 0);
 
 void main(){
   runApp(
@@ -18,12 +18,36 @@ class MyApp extends ConsumerWidget{
   @override
   Widget build(BuildContext context, WidgetRef ref){
     // watch()を使いデータを受け取る
-    // 値は.stateに格納
     final count = ref.watch(counterProvider);
     return Scaffold(
-      body: Center(
-        child:Text('count changed $count'),
+      body: Column(
+        children: [
+          Center(
+            child:
+            Text('count changed $count', style:TextStyle(fontSize:30)),
+          ),
+        ],
       ),
     );
   }
+}
+
+enum BlockMovement {
+  uP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  ROTATE_CLOCKWISE,
+  ROTATE_COUNTER_CLOCKWISE
+}
+
+class Block {
+  late int x;
+  late int y;
+  late Color color;
+  late List<List<int>> _IBlock;
+  Block(this.x, this.y, this.color,_IBlock){
+    _IBlock = [[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]];
+  }
+//動かせるようになったら、Blockを継承で作る。
 }
